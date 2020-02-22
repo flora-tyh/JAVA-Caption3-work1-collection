f4174596ea343272ba9965e676230a2b2b5dfa0e
tyh <756467256@qq.com>
2020-02-22 13:33:59 +0800

    {YunHan}{feat}--{JAVA_caption3_work1}:collection

--- collection/src/main/java/com/thoughtworks/collection/Practice1.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice1.java
@@ -1,6 +1,6 @@
 package com.thoughtworks.collection;
 
-import java.util.Collection;
+import java.util.*;
 
 public class Practice1 {
     public static void main(String[] args) {
@@ -11,6 +11,11 @@ public class Practice1 {
      *  生成10个0到20的随机整数，要求不能重复
      */
     public static Collection<Integer> randomNumber() {
-        return null;
+        Random r = new Random();
+        HashSet<Integer> randomNumberList = new HashSet<>();
+        while (randomNumberList.size() < 10) {
+            randomNumberList.add(r.nextInt(20) + 1);
+        }
+        return randomNumberList;
     }
 }
--- collection/src/main/java/com/thoughtworks/collection/Practice2.java
+++ collection/src/main/java/com/thoughtworks/collection/Practice2.java
@@ -1,18 +1,36 @@
 package com.thoughtworks.collection;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Practice2 {
 
     public static void main(String[] args) {
         //分别用for循环、增强for循环、迭代器遍历并打印getNumbers返回的List
+        for (int i = 0; i < getNumbers().size(); i++) {
+            System.out.println(getNumbers().get(i));
+        }
+
+        for (int i : getNumbers()) {
+            System.out.println(i);
+        }
+
+        Iterator<Integer> it = getNumbers().iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
     }
 
     /**
      * 创建一个List 然后在List中添加0到100整数
      */
     public static List<Integer> getNumbers() {
-        return null;
+        ArrayList<Integer> numberList = new ArrayList<>();
+        for (int i = 0; i <= 100; i++) {
+            numberList.add(i);
+        }
+        return numberList;
     }
 
 
